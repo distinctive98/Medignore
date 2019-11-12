e72cff4d9f7bcca9b5e78276d68d6a74232c4ffb
HyunSu Yoon <hsyun89@naver.com>
2019-11-12 21:06:55 +0900

    open data request

--- HACKATHON/medignore/urls.py
+++ HACKATHON/medignore/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'medignore'
 
 urlpatterns = [
+    path('result/', views.result, name="result"),
+    path('search/', views.search, name="search"),
     path('temp/', views.temp, name="temp"),
     path('', views.main, name="main"),
 ]
--- HACKATHON/medignore/views.py
+++ HACKATHON/medignore/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from decouple import config
+import requests
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode, quote_plus, unquote
 # Create your views here.
 
 def main(request):
@@ -7,3 +10,44 @@ def main(request):
 
 def temp(request):
     return render(request, 'medignore/temp.html')
+
+def search(request):
+    return render(request, 'medignore/search.html')
+
+def result(request):
+    param = request.GET.get('param') # search.html 에서 GET으로 받은 쿼리들 밑에 넣어줘야함
+
+    serviceKey = config('DATA_API_SERVICEKEY')
+    # url = 'http://apis.data.go.kr/1470000/DURPrdlstInfoService/getPwnmTabooInfoList'
+    # params = {
+    #     'serviceKey' : serviceKey,
+    #     'typeName' : '임부금기',
+    #     'ingrCode':'A005138',
+    #     'itemName' : '세레콕시브', 
+    #     'pageNo' : '1', 
+    #     'numOfRows' : '3',
+    #     }
+    # print(requests.get(url,params=params).url)
+    # response = requests.get(url, params=params)
+    # print(response.text)
+
+    API_Key = unquote(serviceKey)
+    url = 'http://apis.data.go.kr/1470000/DURPrdlstInfoService/getPwnmTabooInfoList'
+    queryParams = '?' + urlencode(
+        {
+            quote_plus('serviceKey') : API_Key,
+            quote_plus('typeName'): '임부금기',
+            quote_plus('ingrCode'): 'A005138', 
+            quote_plus('itemName') : '세레콕시브', 
+            quote_plus('pageNo') : '1',
+            quote_plus('numOfRows') : '3',
+            }
+    )
+
+    request = Request(url+queryParams)
+    request.get_method = lambda : 'GET'
+    response_body = urlopen(request).read().decode('utf-8')
+
+    print(response_body)
+
+    # return render(request, 'medignore/result.html',{'param':param})
