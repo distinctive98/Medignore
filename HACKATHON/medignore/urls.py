f27b02c4f00c6d80b2525aaa655d1c3ae97e6494
kyun9 <gusl108@naver.com>
2019-11-21 17:08:47 +0900

    update code

--- HACKATHON/medignore/urls.py
+++ HACKATHON/medignore/urls.py
@@ -5,9 +5,8 @@ app_name = 'medignore'
 
 urlpatterns = [
     path('test/', views.test, name='test'),
-    path('temp/', views.temp, name="temp"),
+    # path('temp/', views.temp, name="temp"),
     path('', views.main, name="main"),
     path('clear/',views.clear_database, name='clear_database'),
-    # path('basic-upload/', views.BasicUploadView, name='basic_upload'),
-    # path('drag-and-drop-upload/',views.DragAndDropUploadView, name='drag_and_drop_upload'),
+    path('result/', views.result, name ='result'),
 ]
--- HACKATHON/medignore/views.py
+++ HACKATHON/medignore/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.views import View
-
+from decouple import config
 from .forms import PhotoForm
 from .models import Photo
 
@@ -20,43 +20,18 @@ LIMIT_BOX = 100
 def main(request):
     return render(request, 'medignore/main.html')
 
-def temp(request):
-    if request.method == 'POST':
-        # imagetest= request.FILES.get('real_path')
-        # print(f'image real_path : {imagetest}')
-        # image='C:/Users/USER/Desktop/aa.PNG'
-        # key = '{key}'
-        # practice(image, key)
-        form = PhotoForm(request.POST, request.FILES)
-        if form.is_valid():
-            photo = form.save()
-            data = {'is_valid': True, 'name': photo.file.name, 'url': photo.file.url}
-            image= './' + photo.file.url
-            # image ='C:/Users/student/Desktop/11.jpg'
-            key = '{key}'
-            output = practice(image, key)
-        else:
-            data = {'is_valid': False}
-        return JsonResponse(data)
-    else:     
-        photos_list = Photo.objects.all()
-        return render(request, 'medignore/drag_and_drop_upload.html', {'photos': photos_list})
 
 def test(request):
-     if request.method == 'POST':
-        # imagetest= request.FILES.get('real_path')
-        # print(f'image real_path : {imagetest}')
-        # image='C:/Users/USER/Desktop/aa.PNG'
-        # key = '{key}'
-        # practice(image, key)
+    kakao_key = config('KAKAO_KEY')
+    if request.method == 'POST':
+       
         form = PhotoForm(request.POST, request.FILES)
         if form.is_valid():
             photo = form.save()
             data = {'is_valid': True, 'name': photo.file.name, 'url': photo.file.url}
             image= './'+photo.file.url
-            # image ='C:/Users/student/Desktop/11.jpg'
             print(f'image url : {image}')
-            key = '{key}'
+            key = kakao_key
             output = practice(image, key)
             resultOutput =output['result']['recognition_words']
             regex =re.compile('\d{9}')
@@ -64,26 +39,15 @@ def test(request):
                 mo = regex.search(item)
                 if mo != None:
                     print(mo.group()) 
-
-            # print(resultOutput)
         else:
             data = {'is_valid': False}
         return JsonResponse(data)
-     else:     
+    else:     
         photos_list = Photo.objects.all()
         return render(request, 'medignore/drag_and_drop_upload.html', {'photos': photos_list})
 
 def kakao_ocr_resize(image_path: str):
-    """
-    ocr detect/recognize api helper
-    ocr api의 제약사항이 넘어서는 이미지는 요청 이전에 전처리가 필요.
-
-    pixel 제약사항 초과: resize
-    용량 제약사항 초과  : 다른 포맷으로 압축, 이미지 분할 등의 처리 필요. (예제에서 제공하지 않음)
-
-    :param image_path: 이미지파일 경로
-    :return:
-    """
+ 
     image = cv2.imread(image_path)
     height, width, _ = image.shape
 
@@ -92,7 +56,6 @@ def kakao_ocr_resize(image_path: str):
         image = cv2.resize(image, None, fx=ratio, fy=ratio)
         height, width, _ = height, width, _ = image.shape
 
-        # api 사용전에 이미지가 resize된 경우, recognize시 resize된 결과를 사용해야함.
         image_path = "{}_resized.jpg".format(image_path)
         cv2.imwrite(image_path, image)
 
@@ -101,11 +64,7 @@ def kakao_ocr_resize(image_path: str):
 
 
 def kakao_ocr_detect(image_path: str, appkey: str):
-    """
-    detect api request example
-    :param image_path: 이미지파일 경로
-    :param appkey: 카카오 앱 REST API 키
-    """
+
     API_URL = 'https://kapi.kakao.com/v1/vision/text/detect'
 
     headers = {'Authorization': 'KakaoAK {}'.format(appkey)}
@@ -118,14 +77,7 @@ def kakao_ocr_detect(image_path: str, appkey: str):
 
 
 def kakao_ocr_recognize(image_path: str, boxes: list, appkey: str):
-    """
-    recognize api request example
-    :param boxes: 감지된 영역 리스트. Canvas 좌표계: 좌상단이 (0,0) / 우상단이 (limit,0)
-                    감지된 영역중 좌상단 점을 기준으로 시계방향 순서, 좌상->우상->우하->좌하
-                    ex) [[[0,0],[1,0],[1,1],[0,1]], [[1,1],[2,1],[2,2],[1,2]], ...]
-    :param image_path: 이미지 파일 경로
-    :param appkey: 카카오 앱 REST API 키
-    """
+ 
     API_URL = 'https://kapi.kakao.com/v1/vision/text/recognize'
 
     headers = {'Authorization': 'KakaoAK {}'.format(appkey)}
@@ -150,43 +102,15 @@ def practice(a, b):
     boxes = output["result"]["boxes"]
     boxes = boxes[:min(len(boxes), LIMIT_BOX)]
     output = kakao_ocr_recognize(image_path, boxes, appkey).json()
-    # print(output)
     return output
-    # print("[recognize] output:\n{}\n".format(json.dumps(output, sort_keys=True, indent=2)))
-
-# class BasicUploadView(View):
-#     def get(self, request):
-#         photos_list = Photo.objects.all()
-#         return render(self.request, 'medignore/temp2.html', {'photos': photos_list})
-
-#     def post(self, request):
-#         form = PhotoForm(self.request.POST, self.request.FILES)
-#         if form.is_valid():
-#             photo = form.save()
-#             data = {'is_valid': True, 'name': photo.file.name, 'url': photo.file.url}
-#         else:
-#             data = {'is_valid': False}
-#         return JsonResponse(data)
-
-
-
-# class DragAndDropUploadView(View):
-#     def get(self, request):
-#         photos_list = Photo.objects.all()
-#         return render(self.request, 'medignore/temp.html', {'photos': photos_list})
-
-#     def post(self, request):
-#         form = PhotoForm(self.request.POST, self.request.FILES)
-#         if form.is_valid():
-#             photo = form.save()
-#             data = {'is_valid': True, 'name': photo.file.name, 'url': photo.file.url}
-#         else:
-#             data = {'is_valid': False}
-#         return JsonResponse(data)
-
+ 
 
 def clear_database(request):
     for photo in Photo.objects.all():
         photo.file.delete()
         photo.delete()
     return redirect(request.POST.get('next'))
+
+
+def result(request):
+    return render(request, 'medignore/result.html')
